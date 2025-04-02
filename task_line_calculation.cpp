82ee79b190aaff63ec0d02d2781368fa129a96cd
Olcay Davut Cabbas <olcay.d.cabbas@gmail.com>
2025-04-02 08:47:26 +0300

    Add files via upload

--- task_line_calculation.cpp
+++ task_line_calculation.cpp
@@ -7,6 +7,7 @@ using namespace task_line_calculation;
 
 int main()
 {
+    //Please add files and subfolders under ./folder directory
     line_calculator calculator(std::filesystem::current_path() / "folder");
     uint64_t total_lines =  calculator.calculate_lines();
 	return 0;
